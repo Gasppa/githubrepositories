bac6ea87e62db3f4a5a079cbde98e14de21321a0
Victor Gasparoni <victorgasparoni@hotmail.com>
2020-10-08 11:01:15 -0300

    Project Finished

--- backend/routes/repositories.js
+++ backend/routes/repositories.js
@@ -4,14 +4,21 @@ module.exports = function(application){
 
     application.post('/repositories', async (req, res)=>{
 
-        const username = req.body.username    
+        const username = req.body.username 
+        let data;   
+        console.log(username)
 
         let url = `https://api.github.com/users/${username}/repos`
         //fazer o get no github e devolver a resposta com os dados da table
 
-        const result = await axios.get(url)
-        const data = await result.data
-
-        res.send(data);
+        try {
+          const result = await axios.get(url)
+          data = await result.data
+          return res.send(data);
+        } 
+        catch (error) {
+          console.log(error)
+          return res.status(400).send('Erro ao realizar consulta')
+        }
     })
 }
